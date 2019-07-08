178bf0bff5d76369c161437c966c8527e609df90
Jonathan Vasquez <jxvo92@yahoo.com>
2019-07-08 01:28:44 +0000

    testing for pushes, inlcluded extra comment in base_model.py

--- console.py
+++ console.py
@@ -32,12 +32,14 @@ class HBNBCommand(cmd.Cmd):
         try:
             if bubbaList[0] == 'BaseModel':
                 x = BaseModel()
+                print(x.id)
+                x.save()
             elif bubbaList[0] == 'User':
                 x = User()
+                print(x.id)
+                x.save()
             """ x = BaseModel() """
             """save id, print id"""
-            x.save()
-            print(x.id)
         except:
             print("** class doesn't exist **")
             print(args)
--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Base Model """
+""" Base Model HELLO"""
 
 import models
 import json
